74609f1a305ae7f81fb6b1acf5ee30d07e92813b
aisyahdz <aisyah.dzulqaidah@vidio.com>
2021-10-30 06:05:10 +0700

    Day 1 HackingWithSwift

--- LearnRxSwift/LearnRxSwiftPlayground.playground/Contents.swift
+++ LearnRxSwift/LearnRxSwiftPlayground.playground/Contents.swift
@@ -1,6 +1,34 @@
 import UIKit
 import RxSwift
 
+//Day 1 hacking with swift
+
+//type inference
+var str  = "Hello world"
+str = "good  morning"
+
+var bigNumber = 8_000_000
+var bigNumberWithWeirdSeparation = 8_0_0_0_0
+
+var multipleLineStringWithLineBreak = """
+    ini adalah
+    contoh multiple line string with line
+    break
+    """
+
+var multipleLineStringWithoutLineBreak = """
+    ini adalah \
+    contoh multiple line string \
+    without line \
+    break
+    """
+
+var double = 3.124
+var interpolation = "jumlah populasi suatu pulau adalah \(bigNumber)"
+var idem = "\(interpolation)"
+
+//type annotation
+let me: String = "Aisyah"
 
 var greeting = "Hello, playground"
 
